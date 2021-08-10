f229b6aa53161021592c5e111240d966121fca71
Michael07220823 <michael31703@gmail.com>
2021-08-10 17:25:22 +0800

    Add pypi generated and uploaded package tools.

---
+++ delete_and_generate_new.sh
@@ -0,0 +1,2 @@
+rm -r dist/ build/ new_tools.egg-info/
+python setup.py sdist bdist_wheel
---
+++ upload_pypi.sh
@@ -0,0 +1,5 @@
+# Test
+# python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* --verbose
+
+# Official
+python -m twine upload dist/* --verbose
